bb2f9bca08448da5de0d1927263263777ffac59d
Chen Liang <code@chen.technology>
2015-03-19 14:08:05 -0700

    [utils] test LocalStack

--- test/test.utils.js
+++ test/test.utils.js
@@ -9,6 +9,7 @@
  * Module dependencies.
  */
 var Promise = require('./../lib/utils').Promise;
+var LocalStack = require('./../lib/utils').LocalStack;
 
 describe('utils', function () {
   describe('.Promise', function () {
@@ -34,6 +35,57 @@ describe('utils', function () {
         q.setTaskId('sometaskId');
         q.taskId.should.equal('sometaskId');
       });
+    });
+  });
+  describe('.LocalStack', function () {
+    var stack;
+    beforeEach(function () {
+      stack = new LocalStack();
+    });
+
+    it('LIFO', function () {
+      stack.push('a');
+      stack.push('b');
+      stack.push('c');
+      stack.pop().should.equal('c');
+    });
+    it('keeps last on the top', function () {
+      stack.push('a');
+      stack.push('b');
+      stack.push('c');
+      stack.top().should.equal('c');
+      stack.pop().should.equal('c');
+      stack.top().should.equal('b');
+      stack.pop().should.equal('b');
+      stack.top().should.equal('a');
+    });
+    it('keeps first on the tail', function () {
+      stack.push('a');
+      stack.push('b');
+      stack.push('c');
+      stack.tail().should.equal('a');
+      stack.pop().should.equal('c');
+      stack.tail().should.equal('a');
+      stack.pop().should.equal('b');
+      stack.tail().should.equal('a');
+    });
+    it('#(size) return stack size', function () {
+      stack.push('a');
+      stack.push('b');
+      stack.push('c');
+      stack.size().should.equal(3);
+      stack.pop();
+      stack.size().should.equal(2);
+      stack.pop();
+      stack.size().should.equal(1);
+    });
+    it('#flush() to empty', function () {
+      stack.push('a');
+      stack.push('b');
+      stack.push('c');
+      stack.size().should.equal(3);
+      stack.flush();
+      stack.size().should.equal(0);
     });
   });
 });
